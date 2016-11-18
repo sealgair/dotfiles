d899d42ca7ac2927e7ab54b920d41afcb3447287
Chase Caster <chasec@darkhorse.com>
2016-11-18 13:46:05 -0800

    add pycharm preferences

---
+++ pycharm/get.sh
@@ -0,0 +1,5 @@
+PYCHARM_DIR=~/Library/Preferences/PyCharm2016.2
+rm -Rf keymaps options
+cp -Rf $PYCHARM_DIR/keymaps .
+cp -Rf $PYCHARM_DIR/options .
+
---
+++ pycharm/set.sh
@@ -0,0 +1,4 @@
+PYCHARM_DIR=~/Library/Preferences/PyCharm2016.2
+cp -Rf keymaps/* $PYCHARM_DIR/keymaps/
+cp -Rf options/* $PYCHARM_DIR/options
+
